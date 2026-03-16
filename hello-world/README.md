ef2c13425a1a9ce0d85f193c38e386c564027f35
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-03-16 10:06:58 +0100

    Hello world!

---
+++ hello-world/README.md
@@ -0,0 +1,8 @@
+# Hello world!
+
+Trying to ptint-out "Hello World!" in the console.
+
+```bash
+java src/main/java/Main.java
+>> Hello world!
+```
---
+++ hello-world/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String texte = "Hello world!";
+        System.out.println(texte);
+    }
+}
