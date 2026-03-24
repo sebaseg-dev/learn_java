7ae7af1b3422cbb73c5fce0a6f31c742710b0cb9
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-03-24 10:48:07 +0100

    adding Player and Hand models

---
+++ oc_project_3-MVC/mvc/src/main/java/fr/sebaseg/cardgame/model/Player.java
@@ -0,0 +1,24 @@
+package fr.sebaseg.cardgame.model;
+
+public class Player {
+    private String name;
+    private Hand hand;
+    
+    public Player(String name) {
+        super();
+        this.name = name;
+        hand = new Hand();
+    }
+    
+    public void addCardToHand(PlayingCard pc) {
+        hand.addCard(pc);
+    }
+    
+    public PlayingCard getCard(int index) {
+        return hand.getCard(index);
+    }
+    
+    public PlayingCard removeCard() {
+        return hand.removeCard();
+    }
+}
