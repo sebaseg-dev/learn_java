6da7c016c3bd9d3fbe95e0e8320014155a7b1161
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-03-24 15:13:40 +0100

    D from SOLID – implementation of a new GUI Interface without needing to change Controller (using a view Interface)

--- oc_project_3-MVC/mvc/src/main/java/fr/sebaseg/cardgame/games/Games.java
+++ oc_project_3-MVC/mvc/src/main/java/fr/sebaseg/cardgame/games/Games.java
@@ -4,11 +4,17 @@ import fr.sebaseg.cardgame.controller.GameController;
 import fr.sebaseg.cardgame.model.Deck;
 import fr.sebaseg.cardgame.view.CommandLineView;
 import fr.sebaseg.cardgame.games.GameEvaluator;
+import fr.sebaseg.cardgame.view.GameSwingView;
 
 public class Games {
     public static void main(String[] args) {
         // GameController gc = new GameController(new Deck(), new View(), new HighCardGameEvaluator());
-        GameController gc = new GameController(new Deck(), new CommandLineView(), new LowCardGameEvaluator());
+        // GameController gc = new GameController(new Deck(), new CommandLineView(), new LowCardGameEvaluator());
+
+        GameSwingView gsv = new GameSwingView();
+        gsv.createAndShowGUI();
+        GameController gc = new GameController(new Deck(), gsv, new LowCardGameEvaluator());
+        
         gc.run();
     }
 }
