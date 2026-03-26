5d6bc3c643156f6ae6e3469112f349e9bb6a979f
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-03-26 10:50:27 +0100

    Add calculation logic for Professional Training Contribution using activity-based rates

--- indie/indie/src/main/java/fr/sebaseg/indie/model/calculators/ProfessionalTrainingContributionCalculator.java
+++ indie/indie/src/main/java/fr/sebaseg/indie/model/calculators/ProfessionalTrainingContributionCalculator.java
@@ -1,4 +1,21 @@
 package fr.sebaseg.indie.model.calculators;
 
+import fr.sebaseg.indie.model.data.BusinessActivity;
+
+import java.math.BigDecimal;
+
 public class ProfessionalTrainingContributionCalculator {
+
+    public BigDecimal getRate(BusinessActivity activity) {
+        return switch (activity.getProfessionalTrainingContribution()) {
+            case SALES -> new BigDecimal("0.001");
+            case ARTISAN -> new BigDecimal("0.003");
+            case LIBERAL -> new BigDecimal("0.002");
+            default -> throw new IllegalArgumentException("ProfessionalTrainingContributionCalculator => Catégorie de Contribution à la Formation Professionnelle inconnue : " + activity.getProfessionalTrainingContribution());
+        };
+    }
+
+    public BigDecimal calculate(BigDecimal turnover, BusinessActivity activity) {
+        return turnover.multiply(getRate(activity));
+    }
 }
