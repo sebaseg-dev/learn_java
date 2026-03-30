7bf9dd260958bfd6137b262bd4aba2f3a977df8b
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-03-30 09:24:04 +0200

    OC P1C2 – Add initial setup for Maven-based testing module with Calculator implementation and JUnit tests

---
+++ oc_project_4-testing/testing/src/test/java/fr/sebaseg/testing/CalculatorTest.java
@@ -0,0 +1,33 @@
+package fr.sebaseg.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void testAddTwoPositiveNumbers() {
+        // Arrange
+        int a = 2;
+        int b = 3;
+        Calculator calculator = new Calculator();
+
+        // Act
+        int somme = calculator.add(a, b);
+
+        // Assert
+        assertEquals(5, somme);
+    }
+
+    @Test
+    void testMultiplyTwoPositiveNumbers() {
+        int a = 2;
+        int b = 3;
+        Calculator calculator = new Calculator();
+
+        int product = calculator.multiply(a, b);
+
+        assertEquals(6, product);
+    }
+}
