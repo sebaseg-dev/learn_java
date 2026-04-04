2e48c9e7a2b9073d1f8b576f0ceeddf170a417c8
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-04-04 18:15:53 +0200

    feat: initialize Spring Boot project with "HelloWorld" application
    - Added a Spring Boot 4.0.5 project generated using Spring Initializr.
    - Included a `HelloworldApplication` class with a main method as the entry point.
    - Added a test class `HelloworldApplicationTests` with a basic context load test.
    - Configured Maven wrapper and project dependencies for Spring Boot and testing.
    - Added `.gitignore` to exclude build results, IDE-specific files, and Maven wrapper artifacts.

---
+++ oc_project_5-Spring_Boot/helloworld_withSpringInitializr/src/test/java/fr/sebaseg/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sebaseg.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
