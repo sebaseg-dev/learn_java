a5c159dbc0e8c3e40245c06f78843b8b5fa36186
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-04-05 14:51:30 +0200

    feat: implement CommandLineRunner to print "Hello World" on application startup

--- oc_project_5-Spring_Boot/helloworld_withSpringInitializr/src/main/java/fr/sebaseg/helloworld/HelloworldApplication.java
+++ oc_project_5-Spring_Boot/helloworld_withSpringInitializr/src/main/java/fr/sebaseg/helloworld/HelloworldApplication.java
@@ -1,13 +1,19 @@
 package fr.sebaseg.helloworld;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloworldApplication {
+public class HelloworldApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
 	}
 
 }
