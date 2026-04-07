6ff1a52c419f1dfa287adf41f2055be5af57c1c5
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-04-07 12:41:42 +0200

    feat: implement employee management functionality with web interface
    - Added `Employee` model and `EmployeeProxy` for REST API interaction.
    - Created `EmployeeService` for business logic handling.
    - Developed `EmployeeController` for web interface and CRUD operations.
    - Implemented Thymeleaf templates (`home.html`, `formNewEmployee.html`, `formUpdateEmployee.html`) for employee views.
    - Added unit test for `EmployeeController` using `MockMvc`.

---
+++ oc_project_5-Spring_Boot/webapp/src/main/java/fr/sebaseg/webapp/repository/EmployeeProxy.java
@@ -0,0 +1,106 @@
+package fr.sebaseg.webapp.repository;
+
+import fr.sebaseg.webapp.CustomProperties;
+import fr.sebaseg.webapp.model.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Component
+public class EmployeeProxy {
+
+    @Autowired
+    private CustomProperties props;
+
+    public Iterable<Employee> getEmployees() {
+        String baseApiUrl = props.getApiUrl();
+        String getEmployeesUrl = baseApiUrl + "/employees";
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Iterable<Employee>> response = restTemplate.exchange(
+                getEmployeesUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                }
+        );
+
+        log.debug("Get Employees call {}", response.getStatusCode());
+
+        return response.getBody();
+    }
+
+    public Employee createEmployee(Employee e) {
+        String baseApiUrl = props.getApiUrl();
+        String createEmployeeUrl = baseApiUrl + "/employee";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Employee> request = new HttpEntity<>(e);
+        ResponseEntity<Employee> response = restTemplate.exchange(
+                createEmployeeUrl,
+                HttpMethod.POST,
+                request,
+                Employee.class
+        );
+
+        log.debug("Create Employee call {}", response.getStatusCode());
+
+        return response.getBody();
+    }
+
+    public Employee getEmployee(int id) {
+        String baseApiUrl = props.getApiUrl();
+        String getEmployeeUrl = baseApiUrl + "/employee/" + id;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Employee> response = restTemplate.exchange(
+                getEmployeeUrl,
+                HttpMethod.GET,
+                null,
+                Employee.class
+        );
+
+        log.debug("Get Employee call {}", response.getStatusCode());
+
+        return response.getBody();
+    }
+
+    public void deleteEmployee(int id) {
+        String baseApiUrl = props.getApiUrl();
+        String deleteEmployeeUrl = baseApiUrl + "/employee/" + id;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Void> response = restTemplate.exchange(
+                deleteEmployeeUrl,
+                HttpMethod.DELETE,
+                null,
+                Void.class
+        );
+
+        log.debug("Delete Employee call {}", response.getStatusCode());
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        String baseApiUrl = props.getApiUrl();
+        String updateEmployeeUrl = baseApiUrl + "/employee/" + employee.getId();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Employee> request = new HttpEntity<>(employee);
+        ResponseEntity<Employee> response = restTemplate.exchange(
+                updateEmployeeUrl,
+                HttpMethod.PUT,
+                request,
+                Employee.class
+        );
+
+        log.debug("Update Employee call {}", response.getStatusCode());
+
+        return response.getBody();
+    }
+}
