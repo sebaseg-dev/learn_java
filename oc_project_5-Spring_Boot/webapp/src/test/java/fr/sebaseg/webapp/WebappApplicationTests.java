b0aaf42838e5cc672185d48cdfb04f2d5def0a5d
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-04-13 16:29:04 +0200

    Reapply "Week4"
    This reverts commit 0e1c06d06374746bde7688b8649390faeb40b6d3.

---
+++ oc_project_5-Spring_Boot/webapp/src/test/java/fr/sebaseg/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sebaseg.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
